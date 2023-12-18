4733a579a644b5b91ee516c9a1cf1945a627c03a
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-18 16:52:25 +0100

    sentry, spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import sentry from "@sentry/astro";
 
-import sitemap from '@astrojs/sitemap';
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    sentry({
+      debug: true,
+      dsn: "https://378cf0c9551d4233a70765ccd6095149@o447951.ingest.sentry.io/4505289071853568",
+      sourceMapsUploadOptions: {
+        project: "javascript-xj",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    spotlightjs(),
+  ],
 });
